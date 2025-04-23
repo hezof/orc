99c348da639e8b14afdb14aaf05db91f1f620d41
jsonhe <jsonhe.hzw@gmail.com>
2025-04-23 14:53:40 +0800

    v0.0.1

--- reader.ext.go
+++ reader.ext.go
@@ -5,3 +5,11 @@ import "github.com/scritchley/orc/proto"
 func (r *Reader) PostScript() *proto.PostScript {
 	return r.postScript
 }
+
+func (r *Reader) Footer() *proto.Footer {
+	return r.footer
+}
+
+func (r *Reader) StripeInformation() *proto.StripeInformation {
+	return r.currentStripeInformation
+}
